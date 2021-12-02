97d51dd68dd43ab4f99dcb2aad423f1a966c8856
Jacob <jacobchanrahan@gmail.com>
2021-12-02 15:40:15 +0000

    Making reader on web

--- fetch/bundle.js
+++ fetch/bundle.js
@@ -1,8 +1,41 @@
 (() => {
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // fetchGithubRepo.js
+  var require_fetchGithubRepo = __commonJS({
+    "fetchGithubRepo.js"(exports, module) {
+      var fetchGithubRepo2 = (repoName2, onDataFetched) => {
+        fetch("https://api.github.com/repos/" + repoName2).then((response) => response.json()).then((jsonData) => {
+          onDataFetched(jsonData);
+        });
+      };
+      module.exports = fetchGithubRepo2;
+    }
+  });
+
   // index.js
+  var fetchGithubRepo = require_fetchGithubRepo();
   var urlInput = document.querySelector("#urlName");
   var button = document.querySelector("#urlSubmit");
+  var repoName = document.querySelector("#repoName");
+  var repoPic = document.querySelector("#repoPic");
+  var repoLink = document.querySelector("#repoLink");
+  var repoStars = document.querySelector("#repoStars");
+  var repoForks = document.querySelector("#repoForks");
+  var repoLan = document.querySelector("#repoLan");
   button.addEventListener("click", () => {
-    console.log(urlInput.value);
+    const apiURL = urlInput.value;
+    console.log(apiURL);
+    fetchGithubRepo(apiURL, (response) => {
+      repoName.innerText = response.full_name;
+      repoPic.innerText = response.owner.avatar_url;
+      repoLink.innerText = response.html_url;
+      repoStars.innerText = response.stargazers_count;
+      repoForks.innerText = response.forks;
+      repoLan.innerText = response.language;
+      console.log(JSON.stringify(response));
+    });
   });
 })();
--- fetch/index.js
+++ fetch/index.js
@@ -1,6 +1,27 @@
+const fetchGithubRepo = require('./fetchGithubRepo')
+
 const urlInput = document.querySelector("#urlName");
 const button = document.querySelector("#urlSubmit");
+const repoName = document.querySelector("#repoName");
+const repoPic = document.querySelector("#repoPic");
+const repoLink = document.querySelector("#repoLink");
+const repoStars = document.querySelector("#repoStars");
+const repoForks = document.querySelector("#repoForks");
+const repoLan = document.querySelector("#repoLan");
+
 
 button.addEventListener("click", () => {
-  console.log(urlInput.value);
+  const apiURL = urlInput.value;
+  console.log(apiURL);
+  fetchGithubRepo(apiURL, (response) => { 
+    repoName.innerText = response.full_name
+    repoPic.innerText = response.owner.avatar_url
+    repoLink.innerText = response.html_url
+    repoStars.innerText = response.stargazers_count
+    repoForks.innerText = response.forks
+    repoLan.innerText = response.language
+
+    console.log(JSON.stringify(response))
+  }); 
 });
+
