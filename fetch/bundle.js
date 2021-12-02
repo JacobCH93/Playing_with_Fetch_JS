32bfe8aa6133df1fc4bca0b69669009f323106ac
Jacob <jacobchanrahan@gmail.com>
2021-12-02 14:19:49 +0000

    Fetch so far

---
+++ fetch/bundle.js
@@ -0,0 +1,24 @@
+(() => {
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[Object.keys(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // fetchGithubRepo.js
+  var require_fetchGithubRepo = __commonJS({
+    "fetchGithubRepo.js"(exports, module) {
+      var fetchGithubRepo2 = (repoName, onDataFetched) => {
+        fetch("https://api.github.com/repos/" + repoName).then((response) => response.json()).then((jsonData) => {
+          onDataFetched(jsonData);
+        });
+      };
+      module.exports = fetchGithubRepo2;
+    }
+  });
+
+  // index.js
+  var fetchGithubRepo = require_fetchGithubRepo();
+  fetchGithubRepo("sinatra/sinatra", (repoData) => {
+    console.log(repoData);
+    document.write(JSON.stringify(repoData));
+  });
+})();
---
+++ fetch/index.js
@@ -0,0 +1,8 @@
+const fetchGithubRepo = require('./fetchGithubRepo');
+
+fetchGithubRepo('sinatra/sinatra', (repoData) => {
+    console.log(repoData);
+    document.write(JSON.stringify(repoData));
+});
+ 
+
